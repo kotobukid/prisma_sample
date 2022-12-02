1014b4058de194d3375fc7324e17143e3e4147f9
kotobukid <kotobuki@daqua.jp>
2022-12-02 14:31:19 +0900

    リフォーマット

--- src/stories/FramedBox.stories.js
+++ src/stories/FramedBox.stories.js
@@ -2,40 +2,40 @@ import FramedBox from '../components/FramedBox.vue';
 
 // More on default export: https://storybook.js.org/docs/vue/writing-stories/introduction#default-export
 export default {
-  title: 'Example/FramedBox',
-  component: FramedBox,
-  // More on argTypes: https://storybook.js.org/docs/vue/api/argtypes
-  argTypes: {
-    // backgroundColor: { control: 'color' },
-    // onClick: {},
-    // size: {
-    //   control: { type: 'select' },
-    //   options: ['small', 'medium', 'large'],
-    // },
-  },
+    title: 'Example/FramedBox',
+    component: FramedBox,
+    // More on argTypes: https://storybook.js.org/docs/vue/api/argtypes
+    argTypes: {
+        // backgroundColor: { control: 'color' },
+        // onClick: {},
+        // size: {
+        //   control: { type: 'select' },
+        //   options: ['small', 'medium', 'large'],
+        // },
+    },
 };
 
 const Template = (args) => ({
-  // Components used in your story `template` are defined in the `components` object
-  components: { FramedBox },
-  // The story's `args` need to be mapped into the template through the `setup()` method
-  setup() {
-    return { args };
-  },
-  // And then the `args` are bound to your component with `v-bind="args"`
-  template: '<framed-box v-bind="args">Sample Text</framed-box>',
+    // Components used in your story `template` are defined in the `components` object
+    components: {FramedBox},
+    // The story's `args` need to be mapped into the template through the `setup()` method
+    setup() {
+        return {args};
+    },
+    // And then the `args` are bound to your component with `v-bind="args"`
+    template: '<framed-box v-bind="args">Sample Text</framed-box>',
 });
 
 export const Primary = Template.bind({});
 // More on args: https://storybook.js.org/docs/vue/writing-stories/args
 Primary.args = {
-  // primary: true,
-  color: 'red',
+    // primary: true,
+    color: 'red',
 };
 
 export const Secondary = Template.bind({});
 Secondary.args = {
-  color: 'blue',
+    color: 'blue',
 };
 // export const Large = Template.bind({});
 // Large.args = {
