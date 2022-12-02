c74e2ab716b45b0f506a3a71f0b0fda5dafaf796
kotobukid <kotobuki@daqua.jp>
2022-12-02 14:18:27 +0900

    コンポーネントを一つ自作して読み込んで見る

---
+++ src/components/FramedBox.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts"></script>
+
+<template lang="pug">
+.framed_box
+    slot
+</template>
+
+<style scoped lang="less">
+.framed_box {
+    outline: 1px solid grey;
+}
+</style>
