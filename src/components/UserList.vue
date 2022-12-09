59a0863366bcdbfa55e8d0f55b131865ba2e8051
kotobukid <kotobuki@daqua.jp>
2022-12-09 19:08:20 +0900

    checkbox特有の扱いづらい点に遭遇しているようだったので改善

--- src/components/UserList.vue
+++ src/components/UserList.vue
@@ -2,6 +2,10 @@
 import {useUsersStore} from "../stores";
 import {computed, ref} from "vue";
 
+interface ChangeEvent<T> {
+    target: { value: T }
+}
+
 const userStore = useUsersStore();
 
 const rev_users = (): void => {
@@ -15,13 +19,17 @@ const sorted_users = computed(() => {
     return local_reverse.value ? [...userStore.users].reverse() : userStore.users;
 })
 
-const local_reverse = ref(false)
+const toggle_local_reverse = (e: ChangeEvent<boolean>) => {
+    local_reverse.value = !local_reverse.value;
+}
+
+const local_reverse = ref(false);
 </script>
 
 <template lang="pug">
 a.button(href="#" @click.prevent="rev_users") Reverse Global
 label
-    input(type="checkbox" v-model="local_reverse")
+    input(type="checkbox" :value="local_reverse" @change.prevent="toggle_local_reverse")
     span Local Reverse
 ul
     li(v-for="u in sorted_users" v-text="`${u.name} (${u.age})`" :key="u.id")
