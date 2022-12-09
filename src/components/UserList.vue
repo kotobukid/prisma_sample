aa11d54e279a6e1e1d9bf21a9223cb98b96aae69
kotobukid <kotobuki@daqua.jp>
2022-12-09 18:12:49 +0900

    rev_users関数内で一時的にリバースする処理を非破壊にした コンポーネント内でリバースする処理を着けてみた

--- src/components/UserList.vue
+++ src/components/UserList.vue
@@ -1,18 +1,28 @@
 <script setup lang="ts">
 import {useUsersStore} from "../stores";
+import {computed, ref} from "vue";
 
 const userStore = useUsersStore();
 
 const rev_users = (): void => {
     alert('反転します(action発行的なものの例)');
-    const reversed_users = userStore.users.reverse();
+    const reversed_users = [...userStore.users].reverse();
     userStore.replace_users(reversed_users);
     // userStore.users = reversed_users;
 }
+
+const sorted_users = computed(() => {
+    return local_reverse.value ? [...userStore.users].reverse() : userStore.users;
+})
+
+const local_reverse = ref(false)
 </script>
 
 <template lang="pug">
-a.button(href="#" @click.prevent="rev_users") Reverse
+a.button(href="#" @click.prevent="rev_users") Reverse Global
+label
+    input(type="checkbox" v-model="local_reverse")
+    span Local Reverse
 ul
-    li(v-for="u in userStore.users" v-text="`${u.name} (${u.age})`" :key="u.id")
+    li(v-for="u in sorted_users" v-text="`${u.name} (${u.age})`" :key="u.id")
 </template>
