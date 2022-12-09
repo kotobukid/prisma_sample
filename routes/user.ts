e3ec923ee61f2c7a1928adae838bfd06e9b09f03
kotobukid <kotobuki@daqua.jp>
2022-12-09 17:34:33 +0900

    tscはprismaのschemeも認識して型エラーを出してくれる模様

--- routes/user.ts
+++ routes/user.ts
@@ -4,9 +4,18 @@ import * as pc from '@prisma/client';
 const router = express.Router();
 const prisma = new pc.PrismaClient();
 
+type User = {
+    id: number,
+    name: string,
+    age: number,
+    email: string | null,
+    updatedAt: Date
+}
+
+
 /* GET users listing. */
 router.get('/', async (req, res, next) => {
-    const users = await prisma.user.findMany();
+    const users: User[] = await prisma.user.findMany();
     res.json({
         users
     });
